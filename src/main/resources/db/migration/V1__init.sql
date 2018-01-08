bebaa086ab26c5967dcbeffd6d807703cb6f8ed5
lionzxy <nikita@kulikof.ru>
2018-01-08 13:06:50 +0300

    Ooops

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -121,7 +121,7 @@ CREATE UNIQUE INDEX messages_id_uindex
 CREATE INDEX messages_pid_threadid_index
   ON messages (parentid, threadid, path, id);
 
-CREATE INDEX messages_pid_threadid_index
+CREATE INDEX messages_pid_threadid_with_zero_index
   ON messages (threadid, path, id) WHERE parentid = 0;
 
 CREATE INDEX messages_pid_created_index
